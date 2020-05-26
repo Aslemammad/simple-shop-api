84693ae3c4f5838fbdb7bcf05bbc705ea82c91b5
M. Bagher Abiat <zorofight94@gmail.com>
2020-05-26 12:50:22 +0430

    index.js completed

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 require('dotenv').config();
 const fastify = require('fastify')({ logger: true });
+const cors = require('fastify-cors');
+const db = require('./database/db');
+const routes = require('./routes/routes');
+
+const app = fastify;
 const env = process.env; // environment variables
+const port = env.PORT || 3000;
+
+// plugins
+app.register(cors);
+app.register(db);
+
+// routes
+routes.map((route) => app.route(route));
+
+const start = async () => {
+	try {
+		await app.listen(port);
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+};
+start();
